af22a20825e3f927167167ec53c7b7185024fe07
stugceates <stugceates@gmail.com>
2023-12-04 16:29:10 +0000

    minor change on ai context

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 function displayInfo(response) {
+  console.log(response.data.answer);
   new Typewriter("#info", {
     strings: response.data.answer,
     autoStart: true,
@@ -14,7 +15,7 @@ function generateInfo(event) {
   let apiKey = "fa2f0ab0044e0f6ed0fo3e30511f6tbc";
   let prompt = `User instructions: give advices about ${instructionsInput.value}`;
   let context =
-    "You are an intelligent world traveller and love give advices to people who on vacation. Your mission is give advices about touristic places. It must contain a must see historical place, a good coffee, breakfast and dinner place, with some historical info about the city. Make it a short, in basic HTML, use <strong> element only and separate each line with a <br/>.";
+    "You are an intelligent world traveller and love give advices to people who on vacation. Your mission is give advices about touristic places. It must contain a must see historical place, a good coffee, breakfast and dinner place, with some historical info about the city. Make it a short, in basic HTML, use <strong> element only and separate each line with a <br>.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   let infoElement = document.querySelector("#info");
   infoElement.style.display = "block";
