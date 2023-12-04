fdc33e73fc1868aecf6b3a935a00b13feb5c3d69
stugceates <stugceates@gmail.com>
2023-12-04 10:18:10 +0000

    add timeout to api

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ function displayInfo(response) {
   new Typewriter("#info", {
     strings: response.data.answer,
     autoStart: true,
-    delay: 5,
+    delay: 20,
     cursor: "",
   });
 }
@@ -12,16 +12,15 @@ function generateInfo(event) {
   event.preventDefault();
 
   let instructionsInput = document.querySelector("#user-instructions");
-
   let apiKey = "fa2f0ab0044e0f6ed0fo3e30511f6tbc";
   let prompt = `User instructions: give advices about ${instructionsInput.value}`;
   let context =
-    "You are an intelligent world traveller and love give advices to people who on vacation. Your mission is give advices about touristic places. It must contain a must see historical place, a good coffee, breakfast and dinner place, with some historical info about the city. Make it a short list.";
+    "You are an intelligent world traveller and love give advices to people who on vacation. Your mission is give advices about touristic places. It must contain a must see historical place, a good coffee, breakfast and dinner place, with some historical info about the city. Make it a short, in basic HTML and separate each line with a <br/>.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   console.log("generating advices");
   console.log(`Prompt ${prompt}`);
   console.log(`Context ${context}`);
-  axios.get(apiUrl).then(displayInfo);
+  axios.get(apiUrl, { timeout: 30000 }).then(displayInfo);
 }
 
 let generatorFormElement = document.querySelector("#ai-generator");
